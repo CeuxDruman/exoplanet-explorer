de2acfa6ab7884f64b9980efeb98913e83722749
Carlos Alberto Mata Gil <carlosalberto.matagil@gmail.com>
2023-05-31 12:36:05 -0400

    Added my solution

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,6 +59,12 @@ Instructions:
     Your code goes here! Uncomment the next line when you're ready to start!
      */
 
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+      .then((response) => {
+        response.results.map((url) => {
+          getJSON(url).then(createPlanetThumb);
+        });
+      });
+    // REMEMBER: As .map is also a Parallel solution, there is no guaranteed that it will be resolved in ORDER. You have to handle it in the front-end.
   });
 })(document);
